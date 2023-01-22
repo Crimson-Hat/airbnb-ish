e8898a7c146c2bf866203f9923a1978ebdda6f11
AlvinWachiraKE <98809053+Crimson-Hat@users.noreply.github.com>
2023-01-22 17:25:16 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;700;800;900&display=swap"
           rel="stylesheet"
         />
-        <title>Airbnb-ish</title>
+        <title>Aloha-BnB</title>
         <meta
           name="description"
           content="Find holiday rentals, cabins, beach houses, unique homes and experiences around the world – all made possible by Hosts on Airbnb."
